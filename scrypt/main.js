ba2c805f8e8835b135137080b1f79493d20dc1ed
cavedium <shports61@gmail.com>
2025-07-26 20:45:52 +0300

    fix burger menu

--- scrypt/main.js
+++ scrypt/main.js
@@ -1,4 +1,9 @@
 document.querySelector('.header__burger').addEventListener('click', function() {
 	this.classList.toggle('active');
 	document.querySelector('.header__menu').classList.toggle('open')
+	if (document.querySelector('.header__menu').classList.contains('open')) {
+		document.body.classList.add('no-scroll')
+	} else {
+		document.body.classList.remove('no-scroll')
+	}
 })
